8131ca301327a1f84e5b6745d3c1dbf3df531d5b
Marco (Pakito) André <marco@7digitaltech.com.br>
2026-01-14 13:30:56 -0300

    add lvl for info route

--- app/Domain/Services/Campaigns/GetCampaignCharacterInfosService.php
+++ app/Domain/Services/Campaigns/GetCampaignCharacterInfosService.php
@@ -33,6 +33,7 @@ class GetCampaignCharacterInfosService
         $campaignCharacterId = (int) $campaignCharacter['id'];
 
         return [
+            'level' => (int) $campaignCharacter['level'],
             'perks' => count(
                 $perkRepo->findByCampaignCharacter($campaignCharacterId)
             ),
